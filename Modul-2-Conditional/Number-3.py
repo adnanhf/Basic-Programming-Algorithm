8ad3852587d46b2f553c1febca9cac99baebfc44
Adnan H.F <adnanhasanfalah@gmail.com>
2018-02-23 14:51:19 +0700

    Create Number-3.py

---
+++ Modul-2-Conditional/Number-3.py
@@ -0,0 +1,11 @@
+nilai = int(input("Masukkan nilai : "))
+if 80<nilai<=100 :
+	print("Indeks : A")
+elif 65<nilai<=80:
+	print("Indeks : B")
+elif 60<nilai<=65:
+	print("Indeks : C")
+elif 0<nilai<=60:
+	print("Indeks : D")
+else:
+	print("nilai tidak dapat ditukar ke Indeks")
