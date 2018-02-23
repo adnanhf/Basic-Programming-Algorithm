2b4fbc1fc657e64250c4a284da64409810497a79
Adnan H.F <adnanhasanfalah@gmail.com>
2018-02-23 09:37:14 +0700

    Delete Number-4.py

--- Modul-1/Number-4.py
+++
@@ -1,7 +0,0 @@
-name=input('Nama: ')
-position=input('Jabatan: ')
-salary=input('Gaji: ')
-print('\n\n Nama:',name,'\n','Jabatan:',position,'\n','Gaji: Rp'+salary+',00 \n')
-print('\n\n')
-print('Pegawai dengan nama '+name+' dan jabatan '+position+' mendapat gaji sebesar Rp'+salary
-	+',00 \ndan harus membayar pajak sebesar Rp'+str(int(int(salary)*(10/100)))+',00')
