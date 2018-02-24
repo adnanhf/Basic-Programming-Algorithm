eeebc8c74f470152b5f93528bd68d2d805914a92
Adnan H.F <adnanhasanfalah@gmail.com>
2018-02-24 21:03:26 +0700

    Create Number-4.py

---
+++ Modul-3-Looping/Number-4.py
@@ -0,0 +1,6 @@
+i,mywords = 0,input("Masukkan Kata : ")
+while(mywords != "DAP"):
+  mywords = input("Masukkan Kata : ")
+  i+=1
+print('\n')
+print("Selamat kata kunci yang anda tebak benar \n","Banyak kata yang salah diinput : ",i)
