758a04d363fe49e7fe3aacbac4f30d7263565b7c
Adnan H.F <adnanhasanfalah@gmail.com>
2018-03-18 15:22:41 +0700

    Delete view_data_module.py

--- Modul-6-Files and Modular Programming/Example-1/view_data_module.py
+++
@@ -1,6 +0,0 @@
-def tampilkanmenu(listmenu):
-  print('---------------Daftar Menu-------------------\n'+
-  '\t Nama Menu \t Harga\n')
-  for i in range(len(listmenu)):
-    print('\t %s \t Rp%s,00' %(listmenu[i][0],listmenu[i][1]))
-  print('---------------------------------------------\n')
