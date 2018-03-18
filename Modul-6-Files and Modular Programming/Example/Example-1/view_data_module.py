0a12ebaaae62c7912d500ad96f2deebb94b11d39
Adnan H.F <adnanhasanfalah@gmail.com>
2018-03-18 19:33:03 +0700

    Update view_data_module.py

--- Modul-6-Files and Modular Programming/Example/Example-1/view_data_module.py
+++ Modul-6-Files and Modular Programming/Example/Example-1/view_data_module.py
@@ -1,6 +1,8 @@
 def tampilkanmenu(listmenu):
   print('---------------Daftar Menu-------------------\n'+
   '\t Nama Menu \t Harga\n')
+  # \t adalah jenis format print yang sama dengan satu kali tab
   for i in range(len(listmenu)):
+    # selain format(), dapat digunakan %{format} yang dapat dialamatkan ke satu variabel
     print('\t %s \t Rp%s,00' %(listmenu[i][0],listmenu[i][1]))
   print('---------------------------------------------\n')
