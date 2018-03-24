42f54e7468920592274aacb18e33724ebd2fd4be
Adnan H.F <adnanhasanfalah@gmail.com>
2018-03-24 23:38:03 +0700

    Update main.py

--- Modul-6-Files and Modular Programming/Jurnal/Bonus/main.py
+++ Modul-6-Files and Modular Programming/Jurnal/Bonus/main.py
@@ -1,9 +1,3 @@
-# f1 = open('MatrixA.txt','r')
-# f2 = open('MatrixB.txt','r')
-
-# m1,m2=json.load(f1),json.load(f2)
-# result = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*m2)] for X_row in m1]
-# print(m1,'\n',m2,'\n',numpy.shape(m1),numpy.shape(m2),'\n',result)
 import module as m
 
 m1 = m.read('MatrixA')
